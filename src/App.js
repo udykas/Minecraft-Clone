ffeeaacb671b346a95c73f3be4c5cd9b428418e7
udykas <udykas@gmail.com>
2021-04-15 22:36:58 -0400

    Set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from 'react-three-fiber';
+import { Sky } from 'drei';
+import { Vector3 } from 'three';
+import { Physics } from 'use-cannon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Sky sunPosition={new Vector3(100,10,100)} />
+      <ambientLight intensity={0.3} />
+      <pointLight castShadow intensity={0.8} position={[100,100,100]} />
+      <Physics>
+        <Ground />
+        <Player />
+      </Physics>
+    </Canvas>
   );
 }
 
