44e97c7cf40a95628e4c21c1d478aa973cbaada9
udykas <udykas@gmail.com>
2021-05-03 15:31:33 -0400

    Set up camera

--- src/Camera.js
+++ src/Camera.js
@@ -1,13 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useThree } from 'react-three-fiber';
 
 export const Camera = (props) => {
-  const ref = useRef()
-  const { setDefaultCamera } = useThree()
+  const ref = useRef();
+  const { setDefaultCamera } = useThree();
 
   useEffect(() => {
-    setDefaultCamera(ref.current)
-  }, [])
+    setDefaultCamera(ref.current);
+  }, [setDefaultCamera])
 
   return <perspectiveCamera ref={ref} {...props} />
 }
